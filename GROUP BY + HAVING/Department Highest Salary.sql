af6c298b2b9df8a4085c434c82b62b659e65137f
Yash Suryawanshi <141981825+suryawanshi-yash@users.noreply.github.com>
2025-11-05 09:39:20 +0530

    Refactor SQL query to select department and employee salaries

--- GROUP BY + HAVING/Department Highest Salary.sql
+++ GROUP BY + HAVING/Department Highest Salary.sql
@@ -1,3 +1,6 @@
-# Write your MySQL query statement below
-select max(e.salary) from Employee e
-join Department d on 
+select d.Name as Department, e.Name as Employee, Salary from Department d 
+join Employee e
+on e.DepartmentId = d.Id
+where (e.Salary, e.DepartmentId) in
+(select max(e.Salary), e.DepartmentId 
+from Employee e group by e.DepartmentId)
